d74f0c6b5f9f989c18a5c188b87a200ede0be863
MusthafaMnr <musthafaparadan002@gmail.com>
2021-11-15 12:54:53 +0530

    Footer-1st step Complet

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div id="footer" class="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Ma'din Academy</span></strong> . All Rights Reserved
+            </div>
+            {/* <div class="credits">
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div> */}
+        </div>
+        </div>
+
+
+
+    )
+}
+export default Footer;
